80081db646e7a8835ebe26c8288fbb06e1f81203
dfrvieira <dfrvieira@users.noreply.github.com>
2021-11-10 22:12:38 +0000

    update form

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -37,13 +37,43 @@ const Form = ({ currentId, setCurrentId }) => {
     <Paper className={classes.paper}>
       <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
         <Typography variant="h6">{currentId ? `Editing "${post.title}"` : 'Cria uma Memória'}</Typography>
-        <TextField name="creator" variant="outlined" label="Criador" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
-        <TextField name="title" variant="outlined" label="Títlo" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
-        <TextField name="message" variant="outlined" label="Discrição" fullWidth multiline rows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
-        <TextField name="tags" variant="outlined" label="Tags (virgúla separa)" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
+        <TextField 
+        name="creator" 
+        variant="outlined" 
+        label="Criador" 
+        fullWidth value={postData.creator} 
+        onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
+        <TextField 
+        name="title" 
+        variant="outlined" 
+        label="Títlo" 
+        fullWidth value={postData.title} 
+        onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
+        <TextField 
+        name="message" 
+        variant="outlined" 
+        label="Discrição" 
+        fullWidth multiline rows={4} value={postData.message} 
+        onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
+        <TextField 
+        name="tags"
+        variant="outlined" 
+        label="Tags (virgúla separa)" 
+        fullWidth value={postData.tags} 
+        onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
         <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} /></div>
-        <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Adicionar</Button>
-        <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Apagar</Button>
+        <Button 
+        className={classes.buttonSubmit} 
+        variant="contained" 
+        color="primary" 
+        size="large" 
+        type="submit" 
+        fullWidth>Adicionar</Button>
+        <Button 
+        variant="contained" 
+        color="secondary" 
+        size="small" 
+        onClick={clear} fullWidth>Apagar</Button>
       </form>
     </Paper>
   );
