e3428e873f97e5b58f2f19d616318bbb4eb76858
dfrvieira <dfrvieira@users.noreply.github.com>
2021-11-11 09:02:08 +0000

    update port server

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://user:Diogo123@cluster0.qamnr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT|| 42080;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`A correr o server: http://localhost:${PORT}`)))
