c076c962bf100b1ba86d90aac4489839c8225db2
dfrvieira <dfrvieira@users.noreply.github.com>
2021-11-14 00:49:09 +0000

    finalupdate

--- server/index.js
+++ server/index.js
@@ -1,24 +1,28 @@
-
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import cors from 'cors';
-
-import postRoutes from './routes/posts.js';
+import cors from 'cors'
 
 const app = express();
+import postRoutes from './routes/posts.js';
+
+const CONNECTION_URL = 'mongodb+srv://user:Diogo123@cluster0.qamnr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+
 app.use(cors());
+app.use(express.json());
+app.use("/posts", postRoutes);
 
-app.use('/posts', postRoutes);
+mongoose
+  .connect(CONNECTION_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("DB Conectada com sucesso!"))
+  .catch((err) => {
+    console.log(err);
+  });
 
-const CONNECTION_URL = 'mongodb+srv://user:Diogo123@cluster0.qamnr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 42080;
-
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`A correr o server: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} não se conectou`));
-
-mongoose.set('useFindAndModify', false);
+app.listen(process.env.PORT || 42080, () => {
+  console.log("Servidor a correr!");
+});
